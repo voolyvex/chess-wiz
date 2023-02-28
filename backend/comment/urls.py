2e2d1223c7acab8dba040945264e81ac1815d020
voolyvex <1joeknapp@gmail.com>
2023-02-28 02:31:33 -0600

    reorder urlpatterns in comment/urls.py + create new Class API view for each request detailed in comment/views.py

--- backend/comment/urls.py
+++ backend/comment/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from comment import views
 
 urlpatterns = [
-    path('', views.CommentList.as_view()),
+    path('<int:pgn_id>/', views.CommentDetailPgn.as_view()),
+    path('<int:fen_id>/', views.CommentDetailFen.as_view()),
+    path('<str:game_id>/', views.CommentDetailGame.as_view()),
     path('post/', views.CommentPost.as_view()),
-    path('<int:pgn_id>/', views.CommentList.as_view()),
-    path('<str:game_id>/', views.CommentList.as_view()),
-    path('<int:fen_id>/', views.CommentList.as_view()),
-    
+    path('', views.CommentList.as_view())
 ]
--- backend/comment/views.py
+++ backend/comment/views.py
@@ -11,31 +11,36 @@ from django.shortcuts import get_object_or_404
 
 
 class CommentList(APIView):
-    @permission_classes([AllowAny])
+    @permission_classes([IsAuthenticated])
     def get(self, request):  # Get all comments
         comments = Comment.objects.all()
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
-
+    
+class CommentDetailPgn(APIView):
     @permission_classes([IsAuthenticated])
     def get(self, request, pgn_id):  # Get comments by pgn_id
         comments = get_object_or_404(Comment, pgn_id__id=pgn_id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
+    
+class CommentDetailFen(APIView):
     @permission_classes([IsAuthenticated])
     def get(self, request, fen_id):  # Get comments by fen_id
         # fen_id_param = request.query_params.get('fen_id')
         comments = get_object_or_404(Comment, fen_id__id=fen_id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
+    
+class CommentDetailGame(APIView):
     @permission_classes([IsAuthenticated])
-    def get(self, request, game_id):  # Get comments by game_id (3rd Party API)
-        game_id_param = request.query_params.get('game_id')
-        comments = Comment.objects.all().filter(game_id=game_id_param)
-        serializer = CommentSerializer(comments, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def get(self, request):  # Get comments by game_id (3rd Party API)
+        if 'game_id' in self.request.GET:
+            game_id_param = self.request.query_params.get('game_id')
+            comments = Comment.objects.filter(game_id = game_id_param)
+            serializer = CommentSerializer(comments, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_418_IM_A_TEAPOT)
 
 
 class CommentPost(APIView):
